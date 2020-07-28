46f9afb059b6983def22e84423e86f0e36a37835
arielk <ar.kruszyn@gmail.com>
2020-07-28 15:41:11 +0300

    weather widget components

---
+++ src/components/WeatherCard/component.js
@@ -0,0 +1,48 @@
+import React from "react";
+import styled from "@emotion/styled";
+import Location from "./location";
+import Condition from "./condition";
+import Icon from "./icon";
+
+const WeatherCard = ({ temp, condition, city, country }) => {
+  let bg;
+  if (temp > 12) {
+    let highColor = (1 - (temp - 12) / 28) * 255;
+    let lowColor = highColor - 150;
+    bg = `linear-gradient(
+      to top,
+      rgb(255, ${highColor}, 0),
+      rgb(255, ${lowColor}, 0)
+    )`;
+  } else {
+    let highColor = (1 - (temp + 20) / 32) * 255;
+    let lowColor = highColor - 150;
+    bg = `linear-gradient(
+      to top,
+      rgb(0, ${highColor}, 255),
+      rgb(0, ${lowColor}, 255)
+    )`;
+  }
+
+  const Card = styled.div`
+    margin: 0 auto;
+    background: ${bg};
+    display: flex;
+    width: 200px;
+    height: 240px;
+    justify-content: space-around;
+    flex-direction: column;
+    align-items: center;
+    border-radius: 15px;
+  `;
+
+  return (
+    <Card>
+      <Location city={city} country={country} />
+      <Icon condition={condition} />
+      <Condition temp={temp} condition={condition} />
+    </Card>
+  );
+};
+
+export default WeatherCard;
---
+++ src/components/WeatherCard/condition.js
@@ -0,0 +1,23 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+const Condition = ({ temp, condition }) => {
+  const Temp = styled.h1`
+    font-family: "Fira Sans", sans-serif;
+    font-size: 2rem;
+  `;
+
+  const State = styled.h3`
+    font-family: "Merriweather", sans-serif;
+    font-weight: 1500;
+  `;
+
+  return (
+    <>
+      <Temp> {temp} C</Temp>
+      <State>{condition}</State>
+    </>
+  );
+};
+
+export default Condition;
